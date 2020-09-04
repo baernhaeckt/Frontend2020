9e0ba2f3feecc1b049b51a3271c38eec52b63f36
Christoph Keller <christoph.keller@bambit.ch>
2020-09-04 22:20:38 +0200

    Initialized MDB.

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,11 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+
+$image-path: '~@/../mdb/mdbvue/img';
+@import '~@/../mdb/mdbvue/scss/mdb-free.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
